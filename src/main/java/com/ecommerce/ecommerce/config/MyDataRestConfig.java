bc5fa1f0b45d36979058a8276d399fb60892085f
e-Ribbit <jstecode@gmail.com>
2023-07-23 15:49:33 +0200

    fixed mistakes plus disable http methods

---
+++ src/main/java/com/ecommerce/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,21 @@
+package com.ecommerce.ecommerce.config;
+
+import com.ecommerce.ecommerce.entity.Product;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        //disable Http Methods for Product: Put, Post and Delete
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
+}
--- src/main/java/com/ecommerce/ecommerce/entity/ProductCategory.java
+++ src/main/java/com/ecommerce/ecommerce/entity/ProductCategory.java
@@ -18,7 +18,7 @@ public class ProductCategory {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "category_names")
+    @Column(name = "category_name")
     private String categoryName;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
